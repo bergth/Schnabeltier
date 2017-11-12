6c759dfe5cbed35d1706d690f01dcdfa74b47504
Theo Berger <theo.berger.20160848@efrei.net>
2017-11-12 11:29:15 +0100

    add Makefiles

---
+++ src/graph_cdb/graph.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list{
+    int id;
+    struct list* next;
+}list;
+
+typedef struct node{
+    int ind;
+    int dead;
+    list* child;
+    list* parents;
+}node;
+
+
+typedef struct graph{
+    int order;
+    int max;
+    node* idTable;
+}graph;
+
+graph* init_graph(int order);
+
+void free_graph(graph** g);
+void free_list(list* l);
+
+void child_born(graph* g, int par1, int par2);
+
+void toDot(const graph* g, const char* filename);
+
+void tmp_init_node(graph* g);
+
+void tmp_inits_nodes(graph* g, int n);
