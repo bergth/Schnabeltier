40e4eced0386a33158f282fc8ed83c4ea32cf122
scorpionsdu78 <fboni.78@gmail.com>
2017-12-02 22:25:40 +0100

    mutation beta

--- src/final/evolution_simulator/mutation.c
+++ src/final/evolution_simulator/mutation.c
@@ -53,9 +53,24 @@ int mutation_discrete(int trait, float independance, int modulo)
 }
 
 
-int mutation(Individu* ind)
+
+void mutation(Individu* ind)
 {
     printf("block");
+
+    ind->couleur = mutation_discrete(ind->couleur,ind->independance,8);
+    ind->teinte = mutation_discrete(ind->teinte,ind->independance,5);
+    ind->longueur_fourrure = mutation_discrete(ind->longueur_fourrure,ind->independance,3);
+    ind->regime = mutation_discrete(ind->regime,ind->independance,3);
+    ind->type_peau = mutation_discrete(ind->type_peau,ind->independance,3);
+    ind->vitesse = mutation_discrete(ind->vitesse,ind->independance,3);
+
+    ind->predation = ind->predation + mutation_continue(ind->independance);
+    ind->survie = ind->survie + mutation_continue(ind->independance);
+    ind->taille = ind->taille + mutation_continue(ind->independance);
+
+
+
 }
 
 
