cfc2778166272712327d14cc03d69220111fba2b
scorpionsdu78 <fboni.78@gmail.com>
2017-12-02 13:22:50 +0100

    mutation beta
    les mutation sont en cour

---
+++ src/final/evolution_simulator/mutation.h
@@ -0,0 +1,11 @@
+#ifndef MUTATION_H_INCLUDED
+#define MUTATION_H_INCLUDED
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "individus.h"
+
+
+#endif // MUTATION_H_INCLUDED
